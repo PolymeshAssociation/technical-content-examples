eac72440fcf762d4b4b72a1123ff010d85b971ca
Xavier Leprêtre <xavier@b9lab.com>
2021-02-03 20:14:03 +0400

    Introduced types for KYC customer info.

---
+++ src/customerInfo.ts
@@ -0,0 +1,31 @@
+export interface ICustomerInfo {
+    name: string
+    country: string
+    passport: string
+    valid: boolean
+    toJSON(): JSON
+}
+
+export class CustomerInfo implements ICustomerInfo {
+    name: string;
+    country: string;
+    passport: string;
+    valid: boolean;
+
+    constructor(info: JSON) {
+        this.name = info["name"]
+        this.country = info["country"]
+        this.passport = info["passport"]
+        this.valid = info["valid"]
+    }
+
+    toJSON(): JSON {
+        return <JSON><unknown>{
+            "name": this.name,
+            "country": this.country,
+            "passport": this.passport,
+            "valid": this.valid
+        }
+    }
+
+}
---
+++ tests/unit/customerInfo.ts
@@ -0,0 +1,38 @@
+import { describe } from 'mocha'
+import { expect } from 'chai'
+import { ICustomerInfo, CustomerInfo } from "../../src/customerInfo"
+
+describe("CustomerInfo Unit Tests", () => {
+
+    it("can construct from JSON", () => {
+        const bareInfo: JSON = <JSON><unknown>{
+            "name": "John Doe",
+            "country": "UK",
+            "passport": "12345",
+            "valid": true
+        }
+        const info = new CustomerInfo(bareInfo)
+
+        expect(info.name).to.equal("John Doe")
+        expect(info.country).to.equal("UK")
+        expect(info.passport).to.equal("12345")
+        expect(info.valid).to.be.true
+    })
+
+    it("can convert to JSON", () => {
+        const bareInfo: JSON = <JSON><unknown>{
+            "name": "John Doe",
+            "country": "UK",
+            "passport": "12345",
+            "valid": true
+        }
+        const info = new CustomerInfo(bareInfo)
+        const back = info.toJSON()
+
+        expect(back["name"]).to.equal("John Doe")
+        expect(back["country"]).to.equal("UK")
+        expect(back["passport"]).to.equal("12345")
+        expect(back["valid"]).to.be.true
+    })
+
+})
