ed58646a7efee1c33f967c19dc1eccc9639bdf7c
Xavier Leprêtre <xavier@b9lab.com>
2021-02-04 20:09:44 +0400

    Added a page for the KYC manager to handle customers.

---
+++ pages/manager.tsx
@@ -0,0 +1,168 @@
+import Head from "next/head"
+import React, { useState } from "react"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+  const [myInfo, setMyInfo] = useState({
+    "id": "",
+    "info": {
+      "name": "",
+      "country": "",
+      "passport": "",
+      "valid": false
+    }
+  } as object);
+
+  function setStatus(content: string) {
+    const element = document.getElementById("status") as HTMLElement
+    element.innerHTML = content
+  }
+
+  function onCustomerIdChanged(e: React.ChangeEvent<HTMLInputElement>): void {
+    setMyInfo({
+      ...myInfo,
+      "id": e.target.value
+    })
+  }
+
+  async function getCustomerInfo(): Promise<Response> {
+    const response = await fetch(`/api/kycCustomer/${myInfo["id"]}`, { "method": "GET" })
+    if (response.status == 404) {
+      setStatus("Customer not found, reload the page and enter your information")
+    } else if (response.status == 200) {
+      setStatus("Info fetched")
+      const body = await response.json()
+      console.log(body);
+      setMyInfo({
+        ...myInfo,
+        "info": body
+      })
+    } else {
+      setStatus("Something went wrong")
+    }
+    return response
+  }
+
+  async function submitGetCustomerInfo(e): Promise<void> {
+    e.preventDefault() // prevent page from submitting form
+    await getCustomerInfo()
+  }
+
+  async function sendValidInfo(valid: boolean): Promise<void> {
+    setMyInfo({
+      ...myInfo,
+      "info": {
+        ...myInfo["info"],
+        "valid": valid
+      }
+    })
+    setStatus("Submitting info")
+    const response = await fetch(`/api/kycCustomer/${myInfo["id"]}`, {
+      "method": "PATCH",
+      "body": JSON.stringify({
+        "valid": valid
+      })
+    })
+    if (response.status == 200) {
+      setStatus("Info submitted and saved")
+    } else {
+      setStatus("Something went wrong")
+      await getCustomerInfo()
+    }
+  }
+
+  async function submitValid(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> {
+    await sendValidInfo((e.target as HTMLButtonElement).getAttribute("data-valid") === "true")
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>EzKyc Manager Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        EzKyc Management Tool
+        </h1>
+
+        <h2>Pick the customer to manage</h2>
+
+        <form lang="en">
+
+          <fieldset>
+            <legend>Id Of Your Customer</legend>
+
+            <div>
+              <label htmlFor="customer-id">Your Id </label>
+              <input name="id" id="customer-id" type="number" placeholder="1" value={myInfo["id"]} onChange={onCustomerIdChanged}></input>
+            </div>
+
+            <div className="submit">
+              <button className="submit customerId" onClick={submitGetCustomerInfo}>Fetch Their Info</button>
+            </div>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>What The Customer Filled In</legend>
+
+            <div>
+              <label htmlFor="customer-name">Their Name </label>
+              <input name="name" id="customer-name" type="text" placeholder="John Doe" value={myInfo["info"]["name"]} readOnly={true}></input>
+            </div>
+
+            <div>
+              <label htmlFor="customer-country">Their Country </label>
+              <input name="country" id="customer-country" type="text" placeholder="UK" value={myInfo["info"]["country"]} readOnly={true}></input>
+            </div>
+
+            <div>
+              <label htmlFor="customer-passport">Their Passport Number </label>
+              <input name="passport" id="customer-passport" type="text" placeholder="12345" value={myInfo["info"]["passport"]} readOnly={true}></input>
+            </div>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>Your Opinion</legend>
+
+            <div>
+              <label htmlFor="customer-valid">Is It Valid </label>
+              <input name="valid" type="checkbox" checked={myInfo["info"]["valid"]} disabled={true} ></input>
+            </div>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>Your Action</legend>
+
+            <div className="submit">
+              <button className="submit" disabled={myInfo["id"] === "" || myInfo["info"]["valid"]} onClick={submitValid} data-valid="true">Validate</button>
+              <button className="submit" disabled={!(myInfo["info"]["valid"])} onClick={submitValid} data-valid="false">Invalidate</button>
+            </div>
+
+          </fieldset>
+
+        </form>
+
+        <div id="status" className={styles.status}>
+          Latest status will show here
+        </div>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
